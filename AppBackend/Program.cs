e2c73736b7e44b66f18eb9d42009b25051e5ddd9
henrrycoronado <coronadovillcahenrryalberto@gmail.com>
2025-05-22 01:50:16 -0400

    clean code

---
+++ AppBackend/Program.cs
@@ -0,0 +1,26 @@
+using AppBackend.Services;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Servicios
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IWeatherService, WeatherService>();
+
+var app = builder.Build();
+
+// Middleware
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
---
+++ AppBackend/README.md
@@ -0,0 +1,18 @@
+Proyecto creado con .net9 usando :
+    - dotnet new webapi -n nombreProyecto
+
+se instala packages con :
+    dotnet add package Swashbuckle.AspNetCore
+    dotnet add package Microsoft.AspNetCore.Authentication.JwtBearer
+
+
+compilacion y ejecucion con:
+    dotnet build
+    dotnet run
+
+
+
+recursos:
+
+usar para ocultar un endpoint de un controller:
+    [ApiExplorerSettings(IgnoreApi = true)]
